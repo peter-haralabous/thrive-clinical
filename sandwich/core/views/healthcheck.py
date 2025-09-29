5a655132b94d410c598f8307bad91e787284d569
Paul Shannon <paul.shannon@thrive.health>
2025-09-29 15:37:34 -0400

    feat: add an optional parameter to healthcheck so that superusers can trigger error codes
    Refs: https://new-hippo.atlassian.net/browse/HC-45

--- sandwich/core/views/healthcheck.py
+++ sandwich/core/views/healthcheck.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.http import HttpResponse
 from django.http import JsonResponse
 from django.utils import timezone
 
@@ -7,4 +8,7 @@ def healthcheck(request):
     """
     Healthcheck endpoint.
     """
+    if request.user.is_superuser:
+        if error_code := request.GET.get("error_code"):
+            return HttpResponse(status_code=int(error_code), content="Simulated error")
     return JsonResponse({"datetime": timezone.now(), "version": settings.APP_VERSION})
