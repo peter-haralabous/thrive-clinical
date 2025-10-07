7697877f915a83ba8c2e2599b9599362eb0a61d7
matteomiceli <omatteomiceli@gmail.com>
2025-10-07 16:41:50 -0700

    feat: add privacy policy to homepage
    https://new-hippo.atlassian.net/browse/HC-39
    
    To bring us in line with Google's [verification
    requirements](https://support.google.com/cloud/answer/13807376)

--- sandwich/core/views/home_test.py
+++ sandwich/core/views/home_test.py
@@ -26,3 +26,14 @@ def test_home_page_buttons(live_server, page: Page):
     # Check that the Continue as Provider button is present and visible
     provider_button = page.get_by_text("Continue as Provider")
     assert provider_button.is_visible()
+
+    # A link on our homepage that takes the user to our privacy policy
+    policy_link = page.get_by_text("Privacy Policy")
+    assert policy_link.is_visible()
+    with page.context.expect_page() as event:
+        policy_link.click()
+        new_tab = event.value
+        new_tab.wait_for_load_state("networkidle")
+        assert new_tab.url == "https://www.thrive.health/privacynotice"
+        policy_header = new_tab.get_by_role("heading", name="PRIVACY NOTICE", exact=True)
+        assert policy_header.is_visible()
