69f23c5af23da935045c486eb8a2c901a6d052e6
Paul Shannon <paul.shannon@thrive.health>
2025-10-07 08:54:48 -0400

    feat: modeladmin
    Refs: https://new-hippo.atlassian.net/browse/HC-35

--- sandwich/core/admin.py
+++ sandwich/core/admin.py
@@ -10,6 +10,25 @@ from sandwich.core.models import Patient
 from sandwich.core.models import Task
 from sandwich.core.models import Template
 
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("slug", "name")
+    prepopulated_fields = {"slug": ["name"]}
+    search_fields = ("name", "slug")
+
+
+@admin.register(Template)
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ("slug", "short_description", "organization")
+    search_fields = ("slug",)
+    list_filter = ("organization__slug",)
+
+    @admin.display(description="Description", boolean=False)
+    def short_description(self, obj):
+        return obj.description.splitlines()[0] if obj.description else ""
+
+
 admin.site.register(
-    [Consent, Document, Encounter, FormioSubmission, Invitation, Organization, Patient, Task, Template],
+    [Consent, Document, Encounter, FormioSubmission, Invitation, Patient, Task],
 )
