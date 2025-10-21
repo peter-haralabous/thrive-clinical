7c124dc380186a323793574af492ae27e8de19bf
Paul Shannon <paul.shannon@thrive.health>
2025-10-21 19:54:19 -0400

    feat: patient_type
    Refs: https://new-hippo.atlassian.net/browse/HC-107

--- sandwich/core/management/types.py
+++ sandwich/core/management/types.py
@@ -1,5 +1,10 @@
 from sandwich.core.management.lib.types import model_type
+from sandwich.core.models import Patient
 from sandwich.core.models import Template
+
+
+def patient_type(value: str) -> Patient:
+    return model_type(Patient, ["id", "email"], value)
 
 
 def template_type(value: str) -> Template:
