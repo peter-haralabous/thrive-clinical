b748eac62ceb6009aeffa7d865519350084e9f34
Paul Shannon <paul.shannon@thrive.health>
2025-10-27 17:23:47 -0400

    feat: UserFactory creates verified email accounts by default
    Refs: https://new-hippo.atlassian.net/browse/HC-117

--- sandwich/users/factories.py
+++ sandwich/users/factories.py
@@ -1,12 +1,19 @@
 from collections.abc import Sequence
 from typing import Any
 
+from allauth.account.models import EmailAddress
 from factory import Faker
 from factory import post_generation
 from factory.django import DjangoModelFactory
 
 from sandwich.core.service.consent_service import record_consent
 from sandwich.users.models import User
+
+
+class EmailAddressFactory(DjangoModelFactory[EmailAddress]):
+    class Meta:
+        model = EmailAddress
+        django_get_or_create = ["user", "email"]
 
 
 class UserFactory(DjangoModelFactory[User]):
@@ -42,6 +49,12 @@ class UserFactory(DjangoModelFactory[User]):
     def consents(self: User, create: bool, extracted: Sequence[Any], **kwargs):  # noqa: FBT001
         if create and extracted:
             record_consent(user=self, decisions=dict.fromkeys(extracted, True))
+
+    @post_generation
+    def email_address(self: User, create: bool, extracted: Sequence[Any], **kwargs):  # noqa: FBT001
+        """Compatibility with allauth.account.EmailAddress; Create primary verified email address for the user."""
+        if create:
+            EmailAddressFactory.create(user=self, email=self.email, primary=True, verified=True)
 
     @classmethod
     def _after_postgeneration(cls, instance, create, results=None):
