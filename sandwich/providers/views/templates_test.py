87b04935770a326dcd756fbfe052290bbbdc4264
Robin Jastrzebski <robin.jastrzebski@thrive.health>
2025-11-04 14:47:24 -0800

    refactor: added typing and use constants
    https://new-hippo.atlassian.net/browse/HC-147

--- sandwich/providers/views/templates_test.py
+++ sandwich/providers/views/templates_test.py
@@ -1,41 +1,51 @@
+from http import HTTPStatus
+
+from django.test import Client
 from django.urls import reverse
 from guardian.shortcuts import remove_perm
 
 from sandwich.core.factories.form import FormFactory
+from sandwich.core.models import Organization
+from sandwich.core.models import Patient
 from sandwich.core.models.role import RoleName
+from sandwich.users.models import User
 
 
-def test_templates_home(client, provider, organization):
+def test_templates_home(client: Client, provider: User, organization: Organization) -> None:
     client.force_login(provider)
     url = reverse("providers:templates_home", kwargs={"organization_id": organization.id})
     response = client.get(url)
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert "provider/templates.html" in [t.name for t in response.templates]
 
 
-def test_templates_home_user_not_in_organization_deny_access(client, user, organization):
+def test_templates_home_user_not_in_organization_deny_access(
+    client: Client, user: User, organization: Organization
+) -> None:
     client.force_login(user)
     url = reverse("providers:templates_home", kwargs={"organization_id": organization.id})
     response = client.get(url)
-    assert response.status_code == 404
+    assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_form_list(client, provider, organization):
+def test_form_list(client: Client, provider: User, organization: Organization) -> None:
     client.force_login(provider)
     url = reverse("providers:form_templates_list", kwargs={"organization_id": organization.id})
     response = client.get(url)
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert "provider/form_list.html" in [t.name for t in response.templates]
 
 
-def test_form_list_user_not_in_organization_deny_access(client, user, organization):
+def test_form_list_user_not_in_organization_deny_access(
+    client: Client, user: User, organization: Organization
+) -> None:
     client.force_login(user)
     url = reverse("providers:form_templates_list", kwargs={"organization_id": organization.id})
     response = client.get(url)
-    assert response.status_code == 404
+    assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_form_list_filters_allowed_forms(client, provider, organization):
+def test_form_list_filters_allowed_forms(client: Client, provider: User, organization: Organization) -> None:
     """Only forms belonging to the organization are shown to a provider.
 
     And only forms that a provider has view_form permission for are shown.
@@ -50,7 +60,7 @@ def test_form_list_filters_allowed_forms(client, provider, organization):
     client.force_login(provider)
     url = reverse("providers:form_templates_list", kwargs={"organization_id": organization.id})
     response = client.get(url)
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     # The forms shown are only for the org the provider is in.
     assert response.context["forms"].object_list == [form1, form2]
 
@@ -64,27 +74,30 @@ def test_form_list_filters_allowed_forms(client, provider, organization):
     assert response.context["forms"].object_list == [form1]
 
 
-def test_form_details(client, provider, organization):
+def test_form_details(client: Client, provider: User, organization: Organization) -> None:
     form = FormFactory.create(organization=organization)
     client.force_login(provider)
     url = reverse("providers:form_template", kwargs={"organization_id": organization.id, "form_id": form.id})
     response = client.get(url)
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert "provider/form_details.html" in [t.name for t in response.templates]
 
 
-def test_form_details_user_not_in_organization_deny_access(client, user, organization):
+def test_form_details_user_not_in_organization_deny_access(
+    client: Client, user: User, organization: Organization
+) -> None:
     form = FormFactory.create(organization=organization)
     client.force_login(user)
     url = reverse("providers:form_template", kwargs={"organization_id": organization.id, "form_id": form.id})
     response = client.get(url)
-    assert response.status_code == 404
+    assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_form_details_patient_deny_access(client, patient, organization):
+def test_form_details_patient_deny_access(client: Client, patient: Patient, organization: Organization) -> None:
     """Patient in org cannot access provider's view of a form template."""
     form = FormFactory.create(organization=organization)
+    assert patient.user is not None
     client.force_login(patient.user)
     url = reverse("providers:form_template", kwargs={"organization_id": organization.id, "form_id": form.id})
     response = client.get(url)
-    assert response.status_code == 404
+    assert response.status_code == HTTPStatus.NOT_FOUND
