e42e6e07df3a252ebc8115a87d794564eb5f5543
Will Fowler <fowlerwill@gmail.com>
2025-11-06 20:22:05 -0700

    fix: close connections on tasks
    Ref: https://new-hippo.atlassian.net/browse/HC-156

--- sandwich/core/util/procrastinate.py
+++ sandwich/core/util/procrastinate.py
@@ -1,8 +1,13 @@
 import functools
+import logging
 
 import pghistory
+from django.db import close_old_connections
+from django.db import reset_queries
 from procrastinate import JobContext
 from procrastinate.contrib.django import app
+
+logger = logging.getLogger(__name__)
 
 
 def define_task(original_func=None, **kwargs):
@@ -10,6 +15,10 @@ def define_task(original_func=None, **kwargs):
     A wrapper around @app.task that adds pghistory context to the task execution.
 
     Any database changes that happen during the task execution will be correlatable.
+
+    This is leveraging a technique from https://github.com/procrastinate-org/procrastinate/issues/1316
+    that purports to resolve our too-many-database-connection issue. It seems that procrastinate
+    and django together have issues closing their connections.
     """
 
     # we always needs `pass_context=True` to get the JobContext,
@@ -19,12 +28,18 @@ def define_task(original_func=None, **kwargs):
     def decorator(func):
         @functools.wraps(func)
         def new_func(context: JobContext, *job_args, **job_kwargs):
-            with pghistory.context(job=context.job.id, task_name=context.job.task_name):
-                if pass_context_to_func:
-                    result = func(context, *job_args, **job_kwargs)
-                else:
-                    result = func(*job_args, **job_kwargs)
-                return result
+            close_old_connections()
+            reset_queries()
+            try:
+                with pghistory.context(job=context.job.id, task_name=context.job.task_name):
+                    if pass_context_to_func:
+                        result = func(context, *job_args, **job_kwargs)
+                    else:
+                        result = func(*job_args, **job_kwargs)
+                    return result
+            finally:
+                close_old_connections()
+                reset_queries()
 
         kwargs["pass_context"] = True
         return app.task(**kwargs)(new_func)
