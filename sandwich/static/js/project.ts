312b3c7ab4ba632de369609255c35e922b36ec97
Diane Tam <diane.tam@thrive.health>
2025-11-18 06:42:34 -0800

    Revert "Merge remote-tracking branch 'sandwich/personal_resizable_panels'"
    This reverts commit 78c7fe69b8a517372c17d944dc6e7777a8ea9045, reversing
    changes made to f4e875090a1e86abf164a5a7a524c7a4e3fee3dc.

--- sandwich/static/js/project.ts
+++ sandwich/static/js/project.ts
@@ -7,7 +7,6 @@ import './components/message-alert';
 import './components/sortable-columns';
 import './components/toggle-container';
 import { initializeDatadog } from './lib/datadog';
-import { initPatientWorkspace } from './patient-workspace';
 
 /* Project specific Javascript goes here. */
 
@@ -26,8 +25,3 @@ try {
 } catch (e) {
   // the user will see UTC dates, oh well.
 }
-
-// Initialize page-specific functionality
-document.addEventListener('DOMContentLoaded', () => {
-  initPatientWorkspace();
-});
