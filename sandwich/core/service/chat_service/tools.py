463c3699a373e18a7bd848f46875db13966b7677
Paul Shannon <paul.shannon@thrive.health>
2025-11-20 12:21:06 -0500

    fix: AI doesn't know about users anymore
    Refs: https://new-hippo.atlassian.net/browse/HC-210

--- sandwich/core/service/chat_service/tools.py
+++ sandwich/core/service/chat_service/tools.py
@@ -1,16 +1,12 @@
 from langchain_core.tools import BaseTool
 
 from sandwich.core.models import Patient
-from sandwich.core.service.tool_service.patient import build_list_patients_tool
 from sandwich.core.service.tool_service.patient import build_patient_record_tool
 from sandwich.users.models import User
 
 
 def get_tools(user: User | None, patient: Patient | None) -> list[BaseTool]:
     tools = []
-    if user:
-        tools.append(build_list_patients_tool(user))
     if user and patient:
-        # tools.append(build_patient_graph_tool(user, patient))
         tools.append(build_patient_record_tool(user, patient))
     return tools
