c1cb441466c00d38925b788e64f1712c9f4d0a14
Paul Shannon <paul.shannon@thrive.health>
2025-11-20 12:14:15 -0500

    feat: current_date_and_time tool
    - the AI is easily confused
    
    Refs: https://new-hippo.atlassian.net/browse/HC-210

--- sandwich/core/service/chat_service/tools.py
+++ sandwich/core/service/chat_service/tools.py
@@ -2,11 +2,12 @@ from langchain_core.tools import BaseTool
 
 from sandwich.core.models import Patient
 from sandwich.core.service.tool_service.patient import build_read_patient_record_tool
+from sandwich.core.service.tool_service.time import current_date_and_time
 from sandwich.users.models import User
 
 
 def get_tools(user: User | None, patient: Patient | None) -> list[BaseTool]:
-    tools = []
+    tools = [current_date_and_time]
     if user and patient:
         tools.append(build_read_patient_record_tool(user, patient))
     return tools
