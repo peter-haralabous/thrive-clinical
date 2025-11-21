1b2b4901aad2fffa75e68bf41d9a286359d0e7f1
Jimmy Fung <jimmy.fung@thrive.health>
2025-11-21 12:13:24 -0800

    chore: add colour override for submit button
    https://new-hippo.atlassian.net/browse/HC-202

--- sandwich/static/js/lib/survey-form-theme.ts
+++ sandwich/static/js/lib/survey-form-theme.ts
@@ -12,6 +12,7 @@ const CustomSandwichTheme: ITheme = {
     '--sjs-primary-backcolor': '#0E44AD', // Thrive Blue
     '--sjs-general-backcolor-dim-light': '#f5f7fa', // background colour for text inputs
     '--sjs-general-backcolor-dim-dark': '#aabacf', // hover colour for buttons, radio inputs, etc
+    '--sjs-primary-backcolor-dark': '#313f54', // hover colour for the submit button
   },
 };
 
